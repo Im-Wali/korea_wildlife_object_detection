7cd271fec38de5b67ed005ea7799385eb1c8d6c7
im-wali <dlawowo01@gmail.com>
2024-11-20 14:24:54 +0900

    수정
    수정

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-# main.py
-from models.create_model import create_model
+import sys
 import yaml
 import os
+from models.create_model import create_model
 
+def load_config(config_path):
+    """설정 파일 로드 함수"""
+    with open(config_path, "r") as f:
+        return yaml.safe_load(f)
 
-# 설정 파일 로드
-with open("config.yaml", "r") as f:
-    config = yaml.safe_load(f)
+def main(config_path="config.yaml"):
+    # 설정 파일 로드
+    config = load_config(config_path)
+    
+    model_params = config["model_params"]
+    training_params = config["training_params"]
 
-model_params = config["model_params"]
-training_params = config["training_params"]
+    # 환경 변수 설정
+    # os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+    os.environ["OMP_NUM_THREADS"] = "1"  # OpenMP에서 사용할 쓰레드 수 제한
 
+    print("Model Parameters:", model_params)
 
-# os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-os.environ["OMP_NUM_THREADS"] = "1"  # OpenMP에서 사용할 쓰레드 수 제한
+    # 모델 생성
+    model = create_model(**model_params)
+    model.load_model()
 
-print(model_params)
+    # 학습 관리 클래스 생성 및 학습 시작
+    model.train()
 
-# 모델 생성
-model = create_model(**model_params)
-model.load_model()
-# 학습 관리 클래스 생성 및 학습 시작
-model.train()
+    # 모델 저장
+    model.save()
 
-model.save()
+if __name__ == "__main__":
+    # 인자값 확인 및 설정 파일 경로 지정
+    config_file = sys.argv[1] if len(sys.argv) > 1 else "config.yaml"
+    
+    # 메인 실행
+    main(config_file)
